dc0f119ac05e5c32ca24a9cd82147f67098929f5
fahad alobaid <fahad.n.b.alobaid@gmail.com>
2022-07-06 15:34:53 +0300

    end task

--- age_calculator.py
+++ age_calculator.py
@@ -1,20 +1,36 @@
+
 from datetime import date
 
 
+
+
+
 def get_dob():
-    # write code here
-	...
+	year = input("Enter year of birth:")
+	month = input("Enter month of birth:")
+	day = input("Enter day of birth:")
+	dob = date({int(year)}/{int(month)}/{int(day)})
+
+	return dob
+
 
 
 def get_age(dob):
-    # write code here
-	...
-
+	current_date = date.today()
+	age = current_date - dob
+	return age
 
 def main():
-	# write code here
-	...
+	dob = get_dob
+	current_date = date.today()
+	age = int((current_date - dob).days/365)
+	if dob <= current_date:
+		print(f"You are {age} years old")
+	else:
+		print("You can't be born in the future")
+  
 
 
 if __name__ == '__main__':
     main()
+
